9d077d0ec8a326285ab13edc879fbe9f8282a60f
Liu Yiqun <liuyiqun01@baidu.com>
2017-08-21 05:46:29 +0000

    Support the build of armeabi.

--- build_android.sh
+++ build_android.sh
@@ -7,8 +7,7 @@ SOURCES_ROOT=$PROJ_ROOT/..
 
 source common.sh
 
-ABI=32
-
+ABI=0
 if [ $# -eq 1 ]; then
     ABI=$1
 fi
@@ -18,26 +17,29 @@ unset OPENBLAS_ROOT
 
 cd $BUILD_ROOT
 if [ $ABI -eq 32 ]; then
+    ANDROID_ABI=armeabi-v7a
     cmake -DCMAKE_INSTALL_PREFIX=$DEST_ROOT \
-          -DTHIRD_PARTY_PATH=$THIRD_PARTY_PATH/armeabi-v7a \
+          -DTHIRD_PARTY_PATH=$THIRD_PARTY_PATH/$ANDROID_ABI \
           -DCMAKE_SYSTEM_NAME=Android \
           -DANDROID_STANDALONE_TOOLCHAIN=$ANDROID_ARM_TOOLCHAIN_ROOT \
-          -DANDROID_ABI=armeabi-v7a \
+          -DANDROID_ABI=$ANDROID_ABI \
           -DANDROID_ARM_NEON=ON \
           -DANDROID_ARM_MODE=ON \
           -DCMAKE_BUILD_TYPE=Release \
           -DWITH_C_API=ON \
+          -DUSE_EIGEN_FOR_BLAS=ON \
           -DWITH_SWIG_PY=OFF \
           -DWITH_GOLANG=OFF \
           -DWITH_STYLE_CHECK=OFF \
           -DCMAKE_PREFIX_PATH="$JUMBO_ROOT" \
           $SOURCES_ROOT
 elif [ $ABI -eq 64 ]; then
+    ANDROID_ABI=arm64-v8a
     cmake -DCMAKE_INSTALL_PREFIX=$DEST_ROOT \
-          -DTHIRD_PARTY_PATH=${THIRD_PARTY_PATH}/arm64-v8a \
+          -DTHIRD_PARTY_PATH=${THIRD_PARTY_PATH}/$ANDROID_ABI \
           -DCMAKE_SYSTEM_NAME=Android \
           -DANDROID_STANDALONE_TOOLCHAIN=$ANDROID_ARM64_TOOLCHAIN_ROOT \
-          -DANDROID_ABI=arm64-v8a \
+          -DANDROID_ABI=$ANDROID_ABI \
           -DANDROID_ARM_MODE=ON \
           -DCMAKE_BUILD_TYPE=Release \
           -DWITH_C_API=ON \
@@ -47,7 +49,21 @@ elif [ $ABI -eq 64 ]; then
           -DCMAKE_PREFIX_PATH="$JUMBO_ROOT" \
           $SOURCES_ROOT
 else
-    echo "Invalid ABI ($ABI), you can set it to 32 or 64."
+    echo "Build for armeabi default."
+    ANDROID_ABI=armeabi
+    cmake -DCMAKE_INSTALL_PREFIX=$DEST_ROOT \
+          -DTHIRD_PARTY_PATH=$THIRD_PARTY_PATH/$ANDROID_ABI \
+          -DCMAKE_SYSTEM_NAME=Android \
+          -DANDROID_STANDALONE_TOOLCHAIN=$ANDROID_ARM_TOOLCHAIN_ROOT \
+          -DANDROID_ABI=$ANDROID_ABI \
+          -DANDROID_ARM_MODE=ON \
+          -DCMAKE_BUILD_TYPE=Release \
+          -DWITH_C_API=ON \
+          -DWITH_SWIG_PY=OFF \
+          -DWITH_GOLANG=OFF \
+          -DWITH_STYLE_CHECK=OFF \
+          -DCMAKE_PREFIX_PATH="$JUMBO_ROOT" \
+          $SOURCES_ROOT
 fi
 
 cd $PROJ_ROOT
