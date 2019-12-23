11eea177deb8995f9c7d927ca2dc6b400399d7b2
Liu Yiqun <liuyiqun01@baidu.com>
2019-12-23 01:22:32 +0000

    Support profiling with gperftools and vtune in run_lite.sh.

--- build_lite.sh
+++ build_lite.sh
@@ -14,7 +14,9 @@ function cmake_gen() {
   source ${PROJ_ROOT}/clear.sh
   cd ${BUILD_ROOT}
   cmake -DTHIRD_PARTY_PATH=${THIRD_PARTY_PATH} \
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo \
         -DWITH_PYTHON=OFF \
+        -DWITH_MKL=ON \
         -DWITH_MKLDNN=OFF \
         -DWITH_LITE=ON \
         -DWITH_TESTING=ON \
@@ -23,6 +25,7 @@ function cmake_gen() {
         -DLITE_WITH_ARM=OFF \
         -DLITE_BUILD_EXTRA=ON \
         -DLITE_WITH_LIGHT_WEIGHT_FRAMEWORK=OFF \
+        -DLITE_WITH_PROFILE=OFF \
         ${SOURCES_ROOT}
   cd ${PROJ_ROOT}
 }
@@ -34,7 +37,9 @@ function build() {
   Building in $BUILD_ROOT
   ============================================
 EOF
-  make -j12
+#  make -j12
+#  make test_step_rnn_lite_x86 VERBOSE=1
+  make test_step_rnn_lite_x86 -j12
   cd $PROJ_ROOT
 }
 
