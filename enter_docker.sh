0064f4ad23efe25769bdfd067ab23da2da93eb7d
Liu Yiqun <liuyiqun01@baidu.com>
2020-12-16 05:42:45 +0000

    Some minor changes.

--- enter_docker.sh
+++ enter_docker.sh
@@ -15,17 +15,25 @@ set -xe
 #export XREKI_IMAGE_TAG=latest-dev-cuda10.1-cudnn7-gcc82
 #export DOCKER_SUFFIX=_dev_cuda${cuda_version}_gcc82
 
-cuda_version=10.0
-export XREKI_IMAGE_NAME=paddlepaddle/paddle_manylinux_devel
-export XREKI_IMAGE_TAG=cuda${cuda_version}_cudnn7
+cuda_version=10.1
+if [ ${cuda_version} == "10.1" ]; then
+  export XREKI_IMAGE_NAME=paddlepaddle/paddle_manylinux_devel
+  export XREKI_IMAGE_TAG=cuda${cuda_version}_cudnn7
+elif [ ${cuda_version} == "11.0" ]; then
+  export XREKI_IMAGE_NAME=tianshuo78520a/paddle_manylinux_devel
+  export XREKI_IMAGE_TAG=cuda${cuda_version}-cudnn8
+fi
+
 export DOCKER_SUFFIX=_manylinux_cuda${cuda_version}
 
+WORK_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}")/../.." && pwd )"
+
 nvidia-docker run --name build_paddle_lyq${DOCKER_SUFFIX} --network=host -it --rm \
-    -v $PWD/../../Paddle:/paddle \
-    -v $PWD/../../models:/models \
-    -v $PWD/../../benchmark:/benchmark \
-    -v $PWD/../../inference:/data \
-    -w /paddle \
+    --privileged \
+    -v $PWD/.vimrc:/root/.vimrc \
+    -v /ssd3/datasets:/data \
+    -v $WORK_ROOT:/work \
+    -w /work \
     $XREKI_IMAGE_NAME:$XREKI_IMAGE_TAG \
     bash
 
--- run_unit_test.sh
+++ run_unit_test.sh
@@ -2,20 +2,21 @@
 
 set -xe
 
-
-export WORK_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
-. ${WORK_ROOT}/env.sh
-BUILD_ROOT=${WORK_ROOT}/build_docker${DOCKER_SUFFIX}
+export PROJ_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
+. ${PROJ_ROOT}/env.sh
 
 set_python_env
 
 export FLAGS_fraction_of_gpu_memory_to_use=0.1
-#export GLOG_v=4
 unset CUDA_VISIBLE_DEVICES
-export CUDA_VISIBLE_DEVICES=1
-export FLAGS_benchmark=1
+export CUDA_VISIBLE_DEVICES="1"
+#export FLAGS_benchmark=1
 
 cd $BUILD_ROOT
-UNIT_TEST_NAME=test_fusion_group_pass
+UNIT_TEST_NAME=test_ir_fusion_group_pass
+#UNIT_TEST_NAME=test_code_generator
+#export GLOG_vmodule=fusion_group_pass=4
+#export GLOG_vmodule=operator=4
+#export GLOG_v=4
 make test ARGS="-R ${UNIT_TEST_NAME} -V"
 
