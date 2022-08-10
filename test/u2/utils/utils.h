54364281346f8a36c513a1c168b8787d2550235a
Hui Zhang <zhtclz@foxmail.com>
2022-08-10 12:17:31 +0000

    frontend

--- test/u2/utils/utils.h
+++ test/u2/utils/utils.h
@@ -6,9 +6,21 @@
 
 namespace ppspeech {
 
+// DISALLOW_COPY_AND_ASSIGN disallows the copy and operator= functions.
+// It goes in the private: declarations in a class.
 #define DISALLOW_COPY_AND_ASSIGN(Type) \
     Type(const Type&) = delete;        \
     Type& operator=(const Type&) = delete;
+
+// A macro to disallow all the implicit constructors, namely the
+// default constructor, copy constructor and operator= functions.
+//
+// This should be used in the private: declarations for a class
+// that wants to prevent anyone from instantiating it. This is
+// especially useful for classes containing only static methods.
+#define DISALLOW_IMPLICIT_CONSTRUCTORS(TypeName) \
+  TypeName();                                    \
+  DISALLOW_COPY_AND_ASSIGN(TypeName)
 
 
 const float kFloatMax = std::numeric_limits<float>::max();
